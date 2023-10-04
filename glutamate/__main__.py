2f72466c7f60ff88bd85ad37fd93d7ddb7fb2797
entressi <entressi@yandex.ru>
2023-10-04 06:09:48 +0300

    Add stub for CLI interface

---
+++ glutamate/__main__.py
@@ -0,0 +1,12 @@
+import sys
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser('glutamate', description='Glutamate CLI. NOT IMLEMENTED YET.')
+    parser.print_help()
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
